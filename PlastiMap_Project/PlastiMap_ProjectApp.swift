244821430d94b7f53865eceb91436fe38fedc310
JPtheOne <juanpimod@gmail.com>
2024-04-09 12:34:54 -0600

    Initial Commit

---
+++ PlastiMap_Project/PlastiMap_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlastiMap_ProjectApp.swift
+//  PlastiMap_Project
+//
+//  Created by iOS Lab on 09/04/24.
+//
+
+import SwiftUI
+
+@main
+struct PlastiMap_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
