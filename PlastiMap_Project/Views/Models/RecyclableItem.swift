a436fd92f3b7c2b7c2ea72a5e1b66b41f8349ccd
JPtheOne <juanpimod@gmail.com>
2024-04-24 15:53:05 -0600

    Now Map_TestView can show single address if provided or show all if not.

---
+++ PlastiMap_Project/Views/Models/RecyclableItem.swift
@@ -0,0 +1,18 @@
+//
+//  RecyclableItem.swift
+//  PlastiMap_Project
+//
+//  Created by iOS Lab on 24/04/24.
+//
+
+import SwiftUI
+
+struct RecyclableItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecyclableItem()
+}
